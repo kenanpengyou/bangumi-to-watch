f880f2873f90ee1619ff93c1f19110860c227eda
liangzhu <Zhu.Liang@cicc.com.cn>
2015-04-22 17:56:38 +0800

    prepared with postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,60 @@
 var gulp = require("gulp");
-var browserSync = require("browser-sync");
+var debug = require('gulp-debug');
+var rename = require("gulp-rename");
+var browserSync = require("browser-sync").create();
 var browserify = require("browserify");
+var postcss = require("gulp-postcss");
+var autoprefixer = require('autoprefixer-core');
+var postcssSimpleVars = require("postcss-simple-vars");
+var postcssMixins = require("postcss-mixins");
+var postcssNested = require("postcss-nested");
+var sourcemaps = require("gulp-sourcemaps");
 var source = require('vinyl-source-stream');
 
+// Css process.
+gulp.task("postcss", function(){
+    var processors = [
+        postcssMixins,
+        postcssSimpleVars,
+        postcssNested,
+        autoprefixer({
+            browsers: ["last 2 versions"]
+        })
+        ],
+    renameConfig = {
+        suffix: "_transformed"
+    };
 
+    return gulp.src("./css/*.css")
+        .pipe(sourcemaps.init())
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write("."))
+        .pipe(rename(renameConfig))
+        .pipe(gulp.dest("./css"));
+});
+
+// Javascript process.
 gulp.task("browserify", function() {
-    return browserify("./app.js")
+    return browserify("./bangumi.js")
         .bundle()
-        .pipe(source("bundle.js"))
+        .pipe(source("bangumi_bundle.js"))
         .pipe(gulp.dest("./"));
 });
 
+gulp.task("js-watch", ["browserify"], browserSync.reload);
 
+
+// Auto refresh.
 gulp.task("browser-sync", function() {
     browserSync({
-        files: ["**"],
-        exclude: ["./idea/**"],
         port: "5000",
         server: {
             baseDir: "./"
         }
     });
+
+    gulp.watch("./javascript/*.js", ["js-watch"]);
+
 });
 
-gulp.task("default", ["browser-sync"]);
+gulp.task("dev", ["browser-sync"]);
